d65dfb11446d138c06d40745dfb0d06b718caf25
Your Name <you@example.com>
2025-03-26 19:00:25 -0300

    new commit add novas coisas

--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -3,12 +3,15 @@ from .models import *
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'homepage.html')
+    banners = Banner.objects.filter(ativo=True)
+    context = { "banners": banners }
+    return render(request, 'homepage.html', context)
 
-def loja(request):
-    produtos = Produto.objects.all() # Query para buscar todos os produtos
+def loja(request, nome_categoria=None):
+    print(nome_categoria)
+    produtos = Produto.objects.filter(ativo=True)
     context = {"produtos": produtos} 
-    return render(request, 'loja.html', context)
+    return render(request, 'loja.html', context=context)
 
 def carrinho(request):
     return render(request, 'carrinho.html')
