9e4bfb6be41afb8e1f57af801ab3af2a5f4bc8e8
Your Name <you@example.com>
2025-03-28 21:21:46 -0300

    new commit adicionando a função do carrinho

--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 # Create your views here.
@@ -18,10 +18,9 @@ def ver_produto(request, id_produto, id_cor=None):
     tem_estoque = False
     cores = {}
     tamanhos = {}
-    nome_cor_selecionada = None
+    cor_selecionada = None
     if id_cor:
-        cor = Cor.objects.get(id=id_cor)
-        nome_cor_selecionada = cor.nome
+        cor_selecionada = Cor.objects.get(id=id_cor)
     produto = Produto.objects.get(id=id_produto)
     itens_estoque = ItemEstoque.objects.filter(produto=produto, quantidade__gt=0)
     if len(itens_estoque) > 0:
@@ -30,8 +29,23 @@ def ver_produto(request, id_produto, id_cor=None):
         if id_cor:
             itens_estoque = ItemEstoque.objects.filter(produto=produto, quantidade__gt=0, cor__id=id_cor)
             tamanhos = {item.tamanho for item in itens_estoque}
-    context = {"produto": produto, "itens_estoque": itens_estoque, "tem_estoque": tem_estoque, "cores": cores, "tamanhos": tamanhos, "nome_cor_selecionada": nome_cor_selecionada}
+    context = {"produto": produto, "tem_estoque": tem_estoque, "cores": cores, "tamanhos": tamanhos, "cor_selecionada": cor_selecionada}
     return render(request, 'ver_produto.html', context)
+
+def adicionar_carrinho(request, id_produto):
+    if request.method == 'POST' and id_produto:
+        dados = request.POST.dict()
+        print(dados)
+        tamanho = dados.get('tamanho')
+        id_cor = dados.get('cor')
+        if not tamanho:
+            return redirect('loja')
+        # Pegar o cliente
+        # Criar o pedido ou pegar o pedido que está em aberto
+        return redirect('carrinho')
+    else:
+        return redirect('loja')
+
 
 def carrinho(request):
     return render(request, 'carrinho.html')
